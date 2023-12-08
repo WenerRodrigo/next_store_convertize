650f43f1e480687c55d5688e6a2da5802321bea5
WenerRodrigo <wener19.wr@gmail.com>
2023-12-08 08:36:52 -0300

    feat - created application header and title

--- src/components/contentHeader/index.tsx
+++ src/components/contentHeader/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BsHandbag } from "react-icons/bs";
 import * as S from './styles';
 
+const logo = require('../../assets/mascara-africana.png');
 
 
 export const ContentHeader = () => {
@@ -15,12 +16,12 @@ export const ContentHeader = () => {
                 <S.IoMdSearchIcon />
             </S.InputContent>
 
-            <S.Title>NEXT STORE</S.Title>
+            <S.Title>NEXT <S.Imagem src={logo} alt='Imagem Logo' /> STORE</S.Title>
 
             <S.Login>Minha conta</S.Login>
 
             {/* talvez envolver com uma div para ter os numeros aqui */}
-            <S.Icon><BsHandbag /></S.Icon>
+            <S.Icon><BsHandbag />0</S.Icon>
         </S.Content>
     )
 }
--- src/components/contentHeader/styles.ts
+++ src/components/contentHeader/styles.ts
@@ -2,46 +2,55 @@ import styled from "styled-components";
 import { IoMdSearch } from "react-icons/io";
 
 export const Content = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly; 
-  width: 100%;
-  padding: 10px; 
-  border-bottom: 1px solid #ccc;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly; 
+    width: 100%;
+    padding: 10px; 
+    border-bottom: 1px solid #ccc;
 `;
 
 export const InputContent = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const Input = styled.input`
-  width: 100%;
-  height: 30px;
-  border: none;
-  border-bottom: 2px solid #000;
-  cursor: pointer;
+    width: 100%;
+    height: 30px;
+    border: none;
+    border-bottom: 2px solid #000;
+    cursor: pointer;
 
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+      outline: none;
+    }
 `;
 
 export const IoMdSearchIcon = styled(IoMdSearch)`
-  width: 20px;
-  height: 20px;
-  margin-left: 5px;
+    position: absolute;
+    width: 18px;
+    height: 20px;
+    right: 0;
 `;
 
 export const Title = styled.h1`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  font-size: 30px;
-  width: 40%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-size: 30px;
+    font-family: 'Bebas Neue', sans-serif;
+    font-weight: bold;
+    letter-spacing: 3px;
+    width: 40%;
 `;
+
+export const Imagem = styled.img`
+    width: 50px;
+`
 
 
 export const Login = styled.span`
